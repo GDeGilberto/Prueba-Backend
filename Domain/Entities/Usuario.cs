3db070bcc61431dbfd3dd4a2a08598c9d3852346
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-23 18:06:01 -0700

    Se crea entidad de usuario

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MinLength(7)]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+        public bool Estatus { get; set; }
+
+        [Required]
+        public SexoEnum Sexo { get; set; }
+
+        public DateTime FechaDeCreacion { get; set; }
+    }
+}
+
