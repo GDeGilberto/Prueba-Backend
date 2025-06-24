cef23a042b877bbaff5560185edb34bf405798a1
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-24 10:10:08 -0700

    Construccion bases de Domain

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -8,24 +8,35 @@ namespace Domain.Entities
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int Id { get; }
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; }
 
         [MinLength(7)]
-        public string NombreUsuario { get; set; }
+        public string NombreUsuario { get; }
 
         [Required]
-        public string Contraseña { get; set; }
-
-        public bool Estatus { get; set; }
+        public string Contraseña { get; }
 
         [Required]
-        public SexoEnum Sexo { get; set; }
+        public EstatusEnum Estatus { get; }
 
-        public DateTime FechaDeCreacion { get; set; }
+        [Required]
+        public SexoEnum Sexo { get; }
+
+        public DateTime FechaDeCreacion { get; }
+
+        public Usuario(string email, string nombreUsuario, string contraseña, EstatusEnum estatus, SexoEnum sexo)
+        {
+            Email = email;
+            NombreUsuario = nombreUsuario;
+            Contraseña = contraseña;
+            Estatus = estatus;
+            Sexo = sexo;
+            FechaDeCreacion = DateTime.Now;
+        }
     }
 }
 
---
+++ Domain/Enum/EstatusEnum.cs
@@ -0,0 +1,8 @@
+namespace Domain.Enum
+{
+    public enum EstatusEnum
+    {
+        Inactivo = 0,
+        Activo = 1
+    }
+}
