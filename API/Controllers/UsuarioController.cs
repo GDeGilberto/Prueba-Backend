87ab63a9ba91f069af266a5231c8db1b049e8efe
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-24 17:38:12 -0700

    Creacion controller de usuario de los endpoints de la api

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,82 @@
+﻿using Application.UseCases;
+using Application.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AddUsuarioUseCase<AddUsuarioRequestDTO> _addUsuarioUseCase;
+        private readonly GetUsuarioUseCase<Usuario, UsuarioResponseDTO> _getUsuarioUseCase;
+        private readonly UpdateUsuarioUseCase _updateUsuarioUseCase;
+        private readonly DeleteUsuarioUseCase _deleteUsuarioUseCase;
+
+        public UsuarioController(
+            AddUsuarioUseCase<AddUsuarioRequestDTO> addUsuarioUseCase,
+            GetUsuarioUseCase<Usuario, UsuarioResponseDTO> getUsuarioUseCase,
+            UpdateUsuarioUseCase updateUsuarioUseCase,
+            DeleteUsuarioUseCase deleteUsuarioUseCase)
+        {
+            _addUsuarioUseCase = addUsuarioUseCase;
+            _getUsuarioUseCase = getUsuarioUseCase;
+            _updateUsuarioUseCase = updateUsuarioUseCase;
+            _deleteUsuarioUseCase = deleteUsuarioUseCase;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsuarioResponseDTO>>> GetUsuarios()
+        {
+            var usuarios = await _getUsuarioUseCase.ExecuteAsync();
+            return Ok(usuarios);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioResponseDTO>> CreateUsuario([FromBody] AddUsuarioRequestDTO usuarioDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var usuario = await _addUsuarioUseCase.ExecuteAsync(usuarioDTO);
+            return CreatedAtAction(nameof(GetUsuarios), new { id = usuario.Id }, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateUsuario(int id, [FromBody] UpdateUsuarioDTO usuarioDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                await _updateUsuarioUseCase.ExecuteAsync(id, usuarioDTO);
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUsuario(int id)
+        {
+            try
+            {
+                await _deleteUsuarioUseCase.ExecuteAsync(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
