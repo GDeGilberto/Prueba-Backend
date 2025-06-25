2007025221e89e7647e3608f7d42f7bbb273ed61
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-24 17:34:51 -0700

    cambios del repository general

--- Application/Interfaces/IRepository.cs
+++ Application/Interfaces/IRepository.cs
@@ -4,8 +4,8 @@
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<bool> DeleteAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+
     }
 }
