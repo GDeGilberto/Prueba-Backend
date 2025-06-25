01a9fe1b60a0cad0cd1649a19ec0f85346756683
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-24 17:37:47 -0700

    Creacion proyecto API

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,13 @@
+using Application.DTOs;
+using Application.Interfaces;
+using Application.Mappers;
+using Application.Presenters;
+using Application.UseCases;
+using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +17,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register Repositories
+builder.Services.AddScoped<IRepository<Usuario>, UsuarioRepository>();
+
+// Register Mappers and Presenters
+builder.Services.AddScoped<IMapper<AddUsuarioRequestDTO, Usuario>, AddUsuarioMapper>();
+builder.Services.AddScoped<IPresenter<Usuario, UsuarioResponseDTO>, UsuarioPresenter>();
+
+// Register Use Cases
+builder.Services.AddScoped<AddUsuarioUseCase<AddUsuarioRequestDTO>>();
+builder.Services.AddScoped<GetUsuarioUseCase<Usuario, UsuarioResponseDTO>>();
+builder.Services.AddScoped<UpdateUsuarioUseCase>();
+builder.Services.AddScoped<DeleteUsuarioUseCase>();
+
 var app = builder.Build();
+
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while creating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
