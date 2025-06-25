88f21ef2d70c937b0e181dcf813f0d657bb78b3f
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2025-06-24 17:36:06 -0700

    creacion del modelo usuario para base datos

---
+++ Infrastructure/Models/UsuarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Enum;
+
+namespace Infrastructure.Models
+{
+    public class UsuarioModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string NombreUsuario { get; set; } = string.Empty;
+
+        [Required]
+        public string Contraseña { get; set; } = string.Empty;
+
+        [Required]
+        public bool Estatus { get; set; } = true;
+
+        [Required]
+        public SexoEnum Sexo { get; set; }
+
+        [Required]
+        public DateTime FechaDeCreacion { get; set; } = DateTime.UtcNow;
+    }
+}
